2cf40e54a7cb251f0190e5d4c3929cff4192d29e
Md Saney Alam <mdsaney.alam@viacommix.com>
2018-09-17 20:59:12 -0400

    commit from Mac

--- project.py
+++ project.py
@@ -1,6 +1,21 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request, redirect, flash
+import os
+import logging
 
 app = Flask(__name__)
+app.config['DEVELOPMENT'] = True
+app.config['SECRET_KEY'] = "super secret key"
+
+
+restaurants=[]
+
+def store_restaurants(name, location):
+    restaurants.append(dict(
+        name = name,
+        location = location
+        )
+    )
+
 
 @app.route('/')
 @app.route('/index')
@@ -10,13 +25,21 @@ def index():
 
 @app.route('/restaurants/')
 def showRestaurants():
-    return render_template('showrestaurants.html')
+    return render_template('showrestaurants.html', restaurants = restaurants)
 
 
-@app.route('/restaurant/new')
+@app.route('/restaurant/new', methods=['GET', 'POST'])
 def createRestaurant():
+    if request.method == 'POST':
+        name = request.form['name']
+        location = request.form['location']
+        store_restaurants(name, location)
+        logging.debug('Stored Restaurant: ' + name)
+        flash("'{}' Restaurant Added.".format(name))
+        return redirect(url_for('showRestaurants'))
     return render_template('createrestaurant.html')
 
 
 if __name__ =="__main__":
-    app.run(debug=True)    
+    app.run(host=os.getenv('IP', '0.0.0.0'), 
+            port=int(os.getenv('PORT', 4444)))  
